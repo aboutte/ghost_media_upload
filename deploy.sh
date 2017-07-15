b7ddc1ea34347d63b50f7f4959e8926a89a59552
Andy Boutte <andy.boutte@reancloud.com>
2017-07-15 15:52:24 -0500

    updated deploy script so it can be executed from anywhere

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+ORIGINAL_DIR="$(pwd)"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
 rm -rf /home/aboutte/ghost_media_upload
 git clone --depth 1 --branch master https://github.com/aboutte/ghost_media_upload.git
