1fc0fa60b00dd78380463e4d77f22c8e61c5aa1e
Andy Boutte <andy.boutte@reancloud.com>
2017-07-15 16:03:35 -0500

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,9 @@ ORIGINAL_DIR="$(pwd)"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-rm -rf /home/aboutte/ghost_media_upload
+DEPLOYMENT_LOCATION="/home/aboutte"
+DEPLOYMENT_DIR="ghost_media_upload"
+
+rm -rf "$DEPLOYMENT_LOCATION/$DEPLOYMENT_DIR"
+cd $DEPLOYMENT_LOCATION
 git clone --depth 1 --branch master https://github.com/aboutte/ghost_media_upload.git
